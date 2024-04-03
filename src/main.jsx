e26867f525aa9b566c19e028878be04b3dcd32b3
Raul <raul.bricio@gmail.com>
2024-04-03 20:32:59 +0200

    030424-2030

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/header/Header.jsx'
-import './index.css'
-import Services from './components/services/Services.jsx'
+import Products from './components/product/Products.jsx'
 import Banner from './components/banner/Banner.jsx'
+import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <Banner />
-    <Services />
-    <Services />
-    <Services />
+    <Products />
+    <Products />
+    <Products />
   </React.StrictMode>,
 )
