29bdf09d5ad29aca1046da6b1d675ac27c3cf74d
Raul <raul.bricio@gmail.com>
2024-04-30 11:20:13 +0200

    Fix image path in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ function Header() {
   <div className="container mx-auto px-4 sm:px-6 lg:px-8">
     <div className="flex justify-between items-center py-3 md:justify-start md:space-x-10">
       <div className="flex justify-start lg:w-0 lg:flex-1">
-        <img className="h-8 w-auto sm:h-10" src='./src/assets/mktdigitak.webp' alt="Logo"/>
+        <img className="h-8 w-auto sm:h-10" src='../src/assets/mktdigitak.webp' alt="Logo"/>
       </div>
-      <nav className="hidden md:flex space-x-10">
+      <nav className="md:flex space-x-10">
         <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
           Inicio
         </Link>
